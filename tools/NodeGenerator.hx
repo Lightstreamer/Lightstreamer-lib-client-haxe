90dc74607e9f297d4615cf46f65bcc96924a953d
acarioni <alessandro.carioni@lightstreamer.com>
2021-12-10 12:00:23 +0100

    refactor build scripts

--- tools/NodeGenerator.hx
+++ tools/NodeGenerator.hx
@@ -1,19 +1,17 @@
 import HxUtils;
 import sys.io.File;
 
-class NodeGenerator {
-  static function main() {
-    copyPackageJson();
-    HxUtils.fixLib("bin/node/lightstreamer-node_orig.js");
-    generateLibs();
-    HxUtils.renameTypescriptDeclarationFile("bin/node/lightstreamer-node_orig.d.ts");
-  }
+function main() {
+  copyPackageJson();
+  HxUtils.fixLib("bin/node/lightstreamer-node_orig.js");
+  generateLibs();
+  HxUtils.renameTypescriptDeclarationFile("bin/node/lightstreamer-node_orig.d.ts");
+}
 
-  static function copyPackageJson() {
-    File.copy("tools/node/package.json", "bin/node/package.json");
-  }
+function copyPackageJson() {
+  File.copy("tools/node/package.json", "bin/node/package.json");
+}
 
-  static function generateLibs() {
-    HxUtils.run("npx rollup --config rollup.config.node.js");
-  }
+function generateLibs() {
+  HxUtils.run("npx rollup --config rollup.config.node.js");
 }
--- tools/WebGenerator.hx
+++ tools/WebGenerator.hx
@@ -1,29 +1,27 @@
 import HxUtils;
 import sys.io.File;
 
-class WebGenerator {
-  static function main() {
-    copyPackageJson();
-    HxUtils.fixLib("bin/web/lightstreamer_orig.js");
-    generateCoreLibs();
-    generateWidgetLib();
-    generateFullLibs();
-    HxUtils.renameTypescriptDeclarationFile("bin/web/lightstreamer_orig.d.ts");
-  }
+function main() {
+  copyPackageJson();
+  HxUtils.fixLib("bin/web/lightstreamer_orig.js");
+  generateCoreLibs();
+  generateWidgetLib();
+  generateFullLibs();
+  HxUtils.renameTypescriptDeclarationFile("bin/web/lightstreamer_orig.d.ts");
+}
 
-  static function copyPackageJson() {
-    File.copy("tools/web/package.json", "bin/web/package.json");
-  }
+function copyPackageJson() {
+  File.copy("tools/web/package.json", "bin/web/package.json");
+}
 
-  static function generateCoreLibs() {
-    HxUtils.run("npx rollup --config rollup.config.core.js");
-  }
+function generateCoreLibs() {
+  HxUtils.run("npx rollup --config rollup.config.core.js");
+}
 
-  static function generateWidgetLib() {
-    HxUtils.run("npx rollup --config rollup.config.widgets.js");
-  }
+function generateWidgetLib() {
+  HxUtils.run("npx rollup --config rollup.config.widgets.js");
+}
 
-  static function generateFullLibs() {
-    HxUtils.run("npx rollup --config rollup.config.full.js");
-  }
+function generateFullLibs() {
+  HxUtils.run("npx rollup --config rollup.config.full.js");
 }
