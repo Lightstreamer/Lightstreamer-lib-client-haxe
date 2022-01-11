dc3d293ed00abfae118f98ae8d4d3e81a6886ab1
acarioni <alessandro.carioni@lightstreamer.com>
2022-01-11 17:08:48 +0100

    add properties to ConnectionDetails

--- src/common/com/lightstreamer/client/ConnectionDetails.hx
+++ src/common/com/lightstreamer/client/ConnectionDetails.hx
@@ -2,40 +2,48 @@ package com.lightstreamer.client;
 
 @:nativeGen
 class ConnectionDetails {
-  @:internal
-  var adapterSet: String;
-  @:internal
   var serverAddress: String;
+  var adapterSet: String;
+  var user: String;
+  var password: String;
+  var sessionId: String;
+  var serverInstanceAddress: String;
+  var serverSocketName: String;
+  var clientIp: String;
 
   public function new() {}
 
-  public function getAdapterSet(): String {
-    return adapterSet;
-  }
-  public function setAdapterSet(adapterSet: String): Void {
-    this.adapterSet = adapterSet;
-  }
   public function getServerAddress(): String {
     return serverAddress;
   }
   public function setServerAddress(serverAddress: String): Void {
     this.serverAddress = serverAddress;
   }
+  public function getAdapterSet(): String {
+    return adapterSet;
+  }
+  public function setAdapterSet(adapterSet: String): Void {
+    this.adapterSet = adapterSet;
+  }
   public function getUser(): String {
-    return null;
+    return user;
   }
-  public function setUser(user: String): Void {}
-  public function getServerInstanceAddress(): String {
-    return null;
+  public function setUser(user: String): Void {
+    this.user = user;
   }
-  public function getServerSocketName(): String {
-    return null;
-  }
-  public function getClientIp(): String {
-    return null;
+  public function setPassword(password: String): Void {
+    this.password = password;
   }
   public function getSessionId(): String {
-    return null;
+    return sessionId;
   }
-  public function setPassword(password: String): Void {}
+  public function getServerInstanceAddress(): String {
+    return serverInstanceAddress;
+  }
+  public function getServerSocketName(): String {
+    return serverSocketName;
+  }
+  public function getClientIp(): String {
+    return clientIp;
+  }
 }
