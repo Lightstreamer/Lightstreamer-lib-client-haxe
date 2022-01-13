770f712e49c34089114a5772dd541959b2cb76e7
acarioni <alessandro.carioni@lightstreamer.com>
2022-01-13 16:35:32 +0100

    implement ConnectionOptions

--- src/common/com/lightstreamer/client/ConnectionOptions.hx
+++ src/common/com/lightstreamer/client/ConnectionOptions.hx
@@ -1,88 +1,180 @@
 package com.lightstreamer.client;
 
 import com.lightstreamer.client.Types;
+import com.lightstreamer.log.LoggerTools;
+using com.lightstreamer.log.LoggerTools;
 
 @:nativeGen
 class ConnectionOptions {
-  public function getConnectTimeout(): String {
-    return null;
-  }
-  public function setConnectTimeout(connectTimeout: String): Void {}
-  public function getCurrentConnectTimeout(): Millis {
-    return 0;
-  }
-  public function setCurrentConnectTimeout(connectTimeout: Millis): Void {}
+  // TODO synchronize methods
+  // TODO fire property listeners
+  var contentLength: ContentLength = new ContentLength(50000000);
+  var firstRetryMaxDelay: Millis = new Millis(100);
+  var forcedTransport: Null<TransportSelection> = null;
+  var httpExtraHeaders: Null<Map<String, String>> = null;
+  var idleTimeout: Millis = new Millis(19000);
+  var keepaliveInterval: Millis = new Millis(0);
+  var requestedMaxBandwidth: RequestedMaxBandwidth = BWUnlimited;
+  var realMaxBandwidth: Null<RealMaxBandwidth> = null;
+  var pollingInterval: Millis = new Millis(0);
+  var reconnectTimeout: Millis = new Millis(3000);
+  var retryDelay: Millis = new Millis(4000);
+  var reverseHeartbeatInterval: Millis = new Millis(0);
+  var sessionRecoveryTimeout: Millis = new Millis(15000);
+  var stalledTimeout: Millis = new Millis(2000);
+  var httpExtraHeadersOnSessionCreationOnly: Bool = false;
+  var serverInstanceAddressIgnored: Bool = false;
+  var slowingEnabled: Bool = false;
+  
+  public function new() {}
+  
   public function getContentLength(): haxe.Int64 {
-    return 0;
+    return contentLength;
   }
-  public function setContentLength(contentLength: haxe.Int64): Void {}
-  public function getFirstRetryMaxDelay(): Millis {
-    return 0;
+  public function setContentLength(contentLength: haxe.Int64): Void {
+    var newValue = ContentLength.fromIntGt0(contentLength);
+    actionLogger.info('contentLength changed: $newValue');
+    this.contentLength = newValue;
   }
-  public function setFirstRetryMaxDelay(firstRetryMaxDelay: Millis): Void {}
-  public function getForcedTransport(): String {
-    return null;
+
+  public function getFirstRetryMaxDelay(): haxe.Int64 {
+    return firstRetryMaxDelay;
   }
-  public function setForcedTransport(forcedTransport: String): Void {}
-  public function getHttpExtraHeaders(): Map<String, String> {
-    return null;
+  public function setFirstRetryMaxDelay(firstRetryMaxDelay: haxe.Int64): Void {
+    var newValue = Millis.fromIntGt0(firstRetryMaxDelay);
+    actionLogger.info('firstRetryMaxDelay changed: $newValue');
+    this.firstRetryMaxDelay = newValue;
   }
-  public function setHttpExtraHeaders(httpExtraHeaders: Map<String, String>): Void {}
-  public function getIdleTimeout(): Millis {
-    return 0;
+
+  public function getForcedTransport(): Null<String> {
+    return forcedTransport;
   }
-  public function setIdleTimeout(idleTimeout: Millis): Void {}
-  public function getKeepaliveInterval(): Millis {
-    return 0;
+  public function setForcedTransport(forcedTransport: Null<String>): Void {
+    var newValue = TransportSelection.fromString(forcedTransport);
+    actionLogger.info('forcedTransport changed: $newValue');
+    this.forcedTransport = newValue;
+    // TODO forward to client
   }
-  public function setKeepaliveInterval(keepaliveInterval: Millis): Void {}
+
+  public function getHttpExtraHeaders(): Null<Map<String, String>> {
+    return httpExtraHeaders;
+  }
+  public function setHttpExtraHeaders(httpExtraHeaders: Null<Map<String, String>>): Void {
+    actionLogger.info('httpExtraHeaders changed: $httpExtraHeaders');
+    this.httpExtraHeaders = httpExtraHeaders;
+  }
+
+  public function getIdleTimeout(): haxe.Int64 {
+    return idleTimeout;
+  }
+  public function setIdleTimeout(idleTimeout: haxe.Int64): Void {
+    var newValue = Millis.fromIntGtEq0(idleTimeout);
+    actionLogger.info('idleTimeout changed: $newValue');
+    this.idleTimeout = newValue;
+  }
+
+  public function getKeepaliveInterval(): haxe.Int64 {
+    return keepaliveInterval;
+  }
+  public function setKeepaliveInterval(keepaliveInterval: haxe.Int64): Void {
+    var newValue = Millis.fromIntGtEq0(keepaliveInterval);
+    actionLogger.info('keepaliveInterval changed: $newValue');
+    this.keepaliveInterval = newValue;
+  }
+
   public function getRequestedMaxBandwidth(): String {
-    return null;
+    return requestedMaxBandwidth.toString();
   }
-  public function setRequestedMaxBandwidth(maxBandwidth: String): Void {}
-  public function getRealMaxBandwidth(): String {
-    return null;
+  public function setRequestedMaxBandwidth(maxBandwidth: String): Void {
+    var newValue = RequestedMaxBandwidthTools.fromString(maxBandwidth);
+    actionLogger.info('keepaliveInterval changed: ${newValue.toString()}');
+    this.requestedMaxBandwidth = newValue;
+    // TODO forward to client
   }
-  public function getPollingInterval(): Millis {
-    return 0;
+
+  public function getRealMaxBandwidth(): Null<String> {
+    return realMaxBandwidth == null ? null : realMaxBandwidth.toString();
   }
-  public function setPollingInterval(pollingInterval: Millis): Void {}
-  public function getReconnectTimeout(): Millis {
-    return 0;
+
+  public function getPollingInterval(): haxe.Int64 {
+    return pollingInterval;
   }
-  public function setReconnectTimeout(reconnectTimeout: Millis): Void {}
-  public function getRetryDelay(): Millis {
-    return 0;
+  public function setPollingInterval(pollingInterval: haxe.Int64): Void {
+    var newValue = Millis.fromIntGtEq0(pollingInterval);
+    actionLogger.info('pollingInterval changed: $newValue');
+    this.pollingInterval = newValue;
   }
-  public function setRetryDelay(retryDelay: Millis): Void {}
-  public function getReverseHeartbeatInterval(): Millis {
-    return 0;
+
+  public function getReconnectTimeout(): haxe.Int64 {
+    return reconnectTimeout;
   }
-  public function setReverseHeartbeatInterval(reverseHeartbeatInterval: Millis): Void {}
-  public function getStalledTimeout(): Millis {
-    return 0;
+  public function setReconnectTimeout(reconnectTimeout: haxe.Int64): Void {
+    var newValue = Millis.fromIntGt0(reconnectTimeout);
+    actionLogger.info('reconnectTimeout changed: $newValue');
+    this.reconnectTimeout = newValue;
   }
-  public function setStalledTimeout(stalledTimeout: Millis): Void {}
-  public function getSessionRecoveryTimeout(): Millis {
-    return 0;
+
+  public function getRetryDelay(): haxe.Int64 {
+    return retryDelay;
   }
-  public function setSessionRecoveryTimeout(sessionRecoveryTimeout: Millis): Void {}
-  public function isEarlyWSOpenEnabled(): Bool {
-    return false;
+  public function setRetryDelay(retryDelay: haxe.Int64): Void {
+    var newValue = Millis.fromIntGt0(retryDelay);
+    actionLogger.info('retryDelay changed: $newValue');
+    this.retryDelay = newValue;
   }
-  public function setEarlyWSOpenEnabled(earlyWSOpenEnabled: Bool): Void {}
+
+  public function getReverseHeartbeatInterval(): haxe.Int64 {
+    return reverseHeartbeatInterval;
+  }
+  public function setReverseHeartbeatInterval(reverseHeartbeatInterval: haxe.Int64): Void {
+    var newValue = Millis.fromIntGtEq0(reverseHeartbeatInterval);
+    actionLogger.info('reverseHeartbeatInterval changed: $newValue');
+    this.reverseHeartbeatInterval = newValue;
+    // TODO forward to client
+  }
+
+  public function getSessionRecoveryTimeout(): haxe.Int64 {
+    return sessionRecoveryTimeout;
+  }
+  public function setSessionRecoveryTimeout(sessionRecoveryTimeout: haxe.Int64): Void {
+    var newValue = Millis.fromIntGtEq0(sessionRecoveryTimeout);
+    actionLogger.info('sessionRecoveryTimeout changed: $newValue');
+    this.sessionRecoveryTimeout = newValue;
+  }
+
+  public function getStalledTimeout(): haxe.Int64 {
+    return stalledTimeout;
+  }
+  public function setStalledTimeout(stalledTimeout: haxe.Int64): Void {
+    var newValue = Millis.fromIntGt0(stalledTimeout);
+    actionLogger.info('stalledTimeout changed: $newValue');
+    this.stalledTimeout = newValue;
+  }
+
   public function isHttpExtraHeadersOnSessionCreationOnly(): Bool {
-    return false;
+    return httpExtraHeadersOnSessionCreationOnly;
   }
-  public function setHttpExtraHeadersOnSessionCreationOnly(httpExtraHeadersOnSessionCreationOnly: Bool): Void {}
+  public function setHttpExtraHeadersOnSessionCreationOnly(httpExtraHeadersOnSessionCreationOnly: Bool): Void {
+    actionLogger.info('httpExtraHeadersOnSessionCreationOnly changed: $httpExtraHeadersOnSessionCreationOnly');
+    this.httpExtraHeadersOnSessionCreationOnly = httpExtraHeadersOnSessionCreationOnly;
+  }
+
   public function isServerInstanceAddressIgnored(): Bool {
-    return false;
+    return serverInstanceAddressIgnored;
   }
-  public function setServerInstanceAddressIgnored(serverInstanceAddressIgnored: Bool): Void {}
+  public function setServerInstanceAddressIgnored(serverInstanceAddressIgnored: Bool): Void {
+    actionLogger.info('serverInstanceAddressIgnored changed: $serverInstanceAddressIgnored');
+    this.serverInstanceAddressIgnored = serverInstanceAddressIgnored;
+  }
+
   public function isSlowingEnabled(): Bool {
-    return false;
+    return slowingEnabled;
   }
-  public function setSlowingEnabled(slowingEnabled: Bool): Void {}
+  public function setSlowingEnabled(slowingEnabled: Bool): Void {
+    actionLogger.info('slowingEnabled changed: $slowingEnabled');
+    this.slowingEnabled = slowingEnabled;
+  }
+
   // TODO javase Proxy
   // public function setProxy(proxy: Proxy): Void {}
 }
--- src/common/com/lightstreamer/client/Types.hx
+++ src/common/com/lightstreamer/client/Types.hx
@@ -2,7 +2,114 @@ package com.lightstreamer.client;
 
 using StringTools;
 
-typedef Millis = haxe.Int64
+abstract Millis(haxe.Int64) to haxe.Int64 {
+  public inline function new(millis) {
+    this = millis;
+  }
+
+  public static function fromIntGt0(millis: haxe.Int64) {
+    if (millis <= 0) {
+      throw new IllegalArgumentException("value must be greater than zero");
+    }
+    return new Millis(millis);
+  }
+
+  public static function fromIntGtEq0(millis: haxe.Int64) {
+    if (millis < 0) {
+      throw new IllegalArgumentException("value must be greater than or equal to zero");
+    }
+    return new Millis(millis);
+  }
+}
+
+abstract ContentLength(haxe.Int64) to haxe.Int64 {
+  public inline function new(length) {
+    this = length;
+  }
+
+  public static function fromIntGt0(millis: haxe.Int64) {
+    if (millis <= 0) {
+      throw new IllegalArgumentException("value must be greater than zero");
+    }
+    return new ContentLength(millis);
+  }
+}
+
+enum abstract TransportSelection(String) to String {
+  var WS = "WS";
+  var WS_STREAMING = "WS-STREAMING";
+  var WS_POLLING = "WS-POLLING";
+  var HTTP = "HTTP";
+  var HTTP_STREAMING = "HTTP-STREAMING";
+  var HTTP_POLLING = "HTTP-POLLING";
+
+  public static function fromString(transport: Null<String>): Null<TransportSelection> {
+    return switch (transport) {
+      case null: null;
+      case "WS": WS;
+      case "WS-STREAMING": WS_STREAMING;
+      case "WS-POLLING": WS_POLLING;
+      case "HTTP": HTTP;
+      case "HTTP-STREAMING": HTTP_STREAMING;
+      case "HTTP-POLLING": HTTP_POLLING;
+      case _: throw new IllegalArgumentException("The given value is not valid. Use one of: HTTP-STREAMING, HTTP-POLLING, WS-STREAMING, WS-POLLING, WS, HTTP or null");
+    };
+  }
+}
+
+abstract ServerAddress(String) to String {
+  public inline function new(address: String) {
+    this = address;
+  }
+
+  public static function fromString(address: Null<String>): Null<ServerAddress> {
+    return switch (address) {
+      case null: null;
+      case s if (s.startsWith("http://") || s.startsWith("https://")): new ServerAddress(s);
+      case _: throw new IllegalArgumentException("address is malformed");
+    }
+  }
+}
+
+@:using(com.lightstreamer.client.Types.RequestedMaxBandwidthTools)
+enum RequestedMaxBandwidth {
+  BWLimited(bw: Float);
+  BWUnlimited;
+}
+
+class RequestedMaxBandwidthTools {
+  public static function fromString(bandwidth: String): RequestedMaxBandwidth {
+    return switch (bandwidth) {
+      case _.toLowerCase() => "unlimited": BWUnlimited;
+      case Std.parseFloat(_) => bw if (!Math.isNaN(bw) && bw > 0): BWLimited(bw);
+      case _: throw new IllegalArgumentException("The given value is a not valid value for RequestedMaxBandwidth. Use a positive number or the string \"unlimited\"");
+    }
+  }
+
+  public static function toString(bandwidth: RequestedMaxBandwidth) {
+    return switch bandwidth {
+      case BWUnlimited: "unlimited";
+      case BWLimited(bw): Std.string(bw);
+    }
+  }
+}
+
+@:using(com.lightstreamer.client.Types.RealMaxBandwidthTools)
+enum RealMaxBandwidth {
+  BWLimited(bw: Float);
+  BWUnlimited;
+  BWUnmanaged;
+}
+
+class RealMaxBandwidthTools {
+  public static function toString(bandwidth: RealMaxBandwidth) {
+    return switch bandwidth {
+      case BWUnlimited: "unlimited";
+      case BWLimited(bw): Std.string(bw);
+      case BWUnmanaged: "unmanaged";
+    }
+  }
+}
 
 typedef ExceptionImpl = #if js
 js.lib.Error
@@ -31,12 +138,3 @@ typedef IllegalArgumentException = cs.system.ArgumentException
 #else
 class IllegalArgumentException extends haxe.Exception {}
 #end
-
-abstract ServerAddress(String) to String {
-  public function new(address: String) {
-    if (!address.startsWith("http://") && !address.startsWith("https://")) {
-      throw new IllegalArgumentException("serverAddress scheme must be http or https");
-    }
-    this = address;
-  }
-}
