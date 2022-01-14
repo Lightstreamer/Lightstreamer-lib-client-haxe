7ae6a01f0ac5d9d0c422854fb54ff438faff1d8c
acarioni <alessandro.carioni@lightstreamer.com>
2022-01-14 09:58:39 +0100

    add php system test

---
+++ test-system/php/main.php
@@ -0,0 +1,29 @@
+<?php
+
+set_include_path(get_include_path().PATH_SEPARATOR.__DIR__.'/../../bin/php/lib');
+spl_autoload_register(
+	function($class){
+		$file = stream_resolve_include_path(str_replace('\\', '/', $class) .'.php');
+		if ($file) {
+			include_once $file;
+		}
+	}
+);
+
+include_once 'QuoteListener.php';
+
+use com\lightstreamer\client\LightstreamerClient;
+use com\lightstreamer\client\Subscription;
+
+$sub = new Subscription('MERGE', ['item1', 'item2', 'item3'], ['stock_name', 'last_price']);
+$sub->setDataAdapter('QUOTE_ADAPTER');
+$sub->setRequestedSnapshot('yes');
+$sub->addListener(new QuoteListener());
+//$client = new LightstreamerClient('http://push.lightstreamer.com','DEMO'); 
+$client = new LightstreamerClient('http://localhost:8080', 'DEMO');
+$client->connect();
+$client->connectionDetails->setUser('foo');
+$client->connectionOptions->setRetryDelay(2000);
+
+assert($client->connectionDetails->getUser() == 'foo');
+assert($client->connectionOptions->getRetryDelay() == 2000);
