4442b91c7861251b8e28e4deb3f45cffc8848e59
acarioni <alessandro.carioni@lightstreamer.com>
2022-02-18 11:28:37 +0100

    add externs for java.crypto

---
+++ src/extern/java/java/javax/crypto/SecretKey.hx
@@ -0,0 +1,5 @@
+package java.javax.crypto;
+
+// see issues https://github.com/HaxeFoundation/haxe/issues/10327 and 
+// https://github.com/HaxeFoundation/hxjava/issues/32
+extern interface SecretKey {}
