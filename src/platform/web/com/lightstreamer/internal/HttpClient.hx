deb036964bc734cb0c34e103a8ec979270d6bd98
acarioni <alessandro.carioni@lightstreamer.com>
2022-02-24 12:11:04 +0100

    implement http and ws clients for web target

---
+++ src/platform/web/com/lightstreamer/internal/HttpClient.hx
@@ -0,0 +1,104 @@
+package com.lightstreamer.internal;
+
+import js.html.XMLHttpRequest;
+import com.lightstreamer.log.LoggerTools;
+using com.lightstreamer.log.LoggerTools;
+
+class HttpClient {
+  final xhr: XMLHttpRequest;
+  final reader = new StreamReader();
+  var isCanceled: Bool = false;
+
+  public function new(url: String, body: String, 
+    headers: Null<Map<String, String>>, 
+    onText: (HttpClient, String)->Void, 
+    onError: (HttpClient, String)->Void, 
+    onDone: HttpClient->Void) {
+    streamLogger.logDebug('HTTP sending: $url $body headers($headers)');
+    this.xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    // set headers
+    if (headers != null) {
+      for (k => v in headers) {
+        xhr.setRequestHeader(k, v);
+      }
+    }
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xhr.send(body);
+    xhr.onreadystatechange = () -> {
+      var state = xhr.readyState;
+      if (state == XMLHttpRequest.HEADERS_RECEIVED) {
+        var status = xhr.status;
+        // in local files, status is 0 upon success in Mozilla Firefox
+        if (!(status == 0 || (status >= 200 && status < 300))) {
+          streamLogger.logDebug('HTTP event: error(HTTP code $status)');
+          onError(this, "Unexpected HTTP code: " + status);
+          xhr.abort();
+        }
+      } else if (state > XMLHttpRequest.HEADERS_RECEIVED) {
+        var lines = if (state == XMLHttpRequest.LOADING) 
+          reader.streamProgress(xhr.responseText);
+        else
+          reader.streamComplete(xhr.responseText);
+        for (line in lines) {
+          if (line == "") continue;
+          streamLogger.logDebug('HTTP event: text($line)');
+          onText(this, line);
+        }
+        if (state == XMLHttpRequest.DONE) {
+          streamLogger.logDebug("HTTP event: complete");
+          onDone(this);
+        }
+      }
+    };
+    xhr.onerror = () -> {
+      var error = "Network error";
+      streamLogger.logDebug('HTTP event: error($error)');
+      onError(this, error);
+    };
+  }
+
+  public function dispose() {
+    streamLogger.logDebug("HTTP disposing");
+    isCanceled = true;
+    xhr.abort();
+  }
+
+  inline public function isDisposed() {
+    return isCanceled;
+  }
+}
+
+class StreamReader {
+  var progress = 0;
+
+  inline public function new() {}
+
+  function extractNewData(stream: String, isComplete: Bool) {
+    var endIndex;
+    if (isComplete) {
+      endIndex = stream.length;
+    } else {
+      endIndex = stream.lastIndexOf("\r\n");
+      if (endIndex < 0) {
+        return [];
+      } else {
+        endIndex += 2;
+      }
+    }
+    if (endIndex <= progress) {
+      return [];
+    }
+    var newData = stream.substring(progress, endIndex);
+    progress = endIndex;
+    return newData.split("\r\n");
+  }
+
+  inline public function streamProgress(stream: String) {
+    return extractNewData(stream, false);
+  }
+
+  inline public function streamComplete(stream: String) {
+    return extractNewData(stream, true);
+  }
+}
---
+++ src/platform/web/com/lightstreamer/internal/WsClient.hx
@@ -0,0 +1,58 @@
+package com.lightstreamer.internal;
+
+import js.html.WebSocket;
+import com.lightstreamer.log.LoggerTools;
+using com.lightstreamer.log.LoggerTools;
+
+class WsClient {
+  final ws: WebSocket;
+  var isCanceled = false;
+
+  public function new(url: String,
+    onOpen: WsClient->Void,
+    onText: (WsClient, String)->Void, 
+    onError: (WsClient, String)->Void) {
+    streamLogger.logDebug('WS connecting: $url');
+    this.ws = new WebSocket(url, Constants.Sec_WebSocket_Protocol);
+    ws.onopen = () -> {
+      streamLogger.logDebug('WS event: open');
+      onOpen(this);
+    };
+    ws.onmessage = evt -> {
+      var text: String = evt.data;
+      for (line in text.split("\r\n")) {
+        if (line == "") continue;
+        streamLogger.logDebug('WS event: text($line)');
+        onText(this, line);
+      }
+    };
+    ws.onerror = () -> {
+      var msg = "Network error";
+      streamLogger.logDebug('WS event: error($msg)');
+      onError(this, msg);
+      ws.close();
+    };
+    ws.onclose = evt -> {
+      if (isCanceled) return;  // closing is expected
+      var msg =  "unexpected disconnection: " + evt.code + " - " + evt.reason;
+      streamLogger.logDebug('WS event: error($msg)');
+      onError(this, msg);
+      ws.close();
+    };
+  }
+
+  public function send(txt: String) {
+    streamLogger.logDebug('WS sending: $txt');
+    ws.send(txt);
+  }
+
+  public function dispose() {
+    streamLogger.logDebug("WS disposing");
+    isCanceled = true;
+    ws.close();
+  }
+
+  inline public function isDisposed() {
+    return isCanceled;
+  }
+}
