e49dea56f3b4ac89ea9b01a78ef32b6654cd804d
acarioni <alessandro.carioni@lightstreamer.com>
2022-03-14 12:20:45 +0100

    fix typo

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -41,7 +41,7 @@ class TestAll {
     runner.addCase(new com.lightstreamer.client.mpn.TestMpnBuilderSafari());
     #end
     #if java
-    runner.addCase(new com.lightstreamer.client.mpn.TestdMpnBuilderAndroid());
+    runner.addCase(new com.lightstreamer.client.mpn.TestMpnBuilderAndroid());
     #end
     #end
   }
