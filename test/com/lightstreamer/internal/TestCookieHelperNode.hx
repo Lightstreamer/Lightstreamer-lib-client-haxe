11364a7ae75075f16a1fbc16e554c1f00a4d8d93
acarioni <alessandro.carioni@lightstreamer.com>
2022-03-14 09:31:24 +0100

    test cookie pathname

--- test/com/lightstreamer/internal/TestCookieHelperNode.hx
+++ test/com/lightstreamer/internal/TestCookieHelperNode.hx
@@ -12,4 +12,16 @@ class TestCookieHelperNode extends utest.Test {
     CookieHelper.instance.clearCookies();
     strictSame([], CookieHelper.instance.getCookies(uri));
   }
+
+  function testCookiesWithPath() {
+    var uri = "http://www.example.com/ls";
+    strictSame([], CookieHelper.instance.getCookies(uri));
+    
+    var cookie = "Foo=bar";
+    CookieHelper.instance.addCookies(uri, [cookie]);
+    strictSame(["Foo=bar; domain=www.example.com; path=/ls"], CookieHelper.instance.getCookies(uri));
+
+    CookieHelper.instance.clearCookies();
+    strictSame([], CookieHelper.instance.getCookies(uri));
+  }
 }
