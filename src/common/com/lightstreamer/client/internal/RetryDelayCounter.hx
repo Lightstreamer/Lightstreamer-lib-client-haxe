b81f3504050c604c86745e001e9477a68144aee9
acarioni <alessandro.carioni@lightstreamer.com>
2022-03-25 17:20:33 +0100

    tweak RetryDelayCounter

--- src/common/com/lightstreamer/client/internal/RetryDelayCounter.hx
+++ src/common/com/lightstreamer/client/internal/RetryDelayCounter.hx
@@ -1,5 +1,6 @@
 package com.lightstreamer.client.internal;
 
+import com.lightstreamer.internal.Types.Millis;
 import com.lightstreamer.internal.NativeTypes.Long;
 
 class RetryDelayCounter {
@@ -7,26 +8,30 @@ class RetryDelayCounter {
   var currentRetryDelay: Long;
 
   public function new() {
-      attempt = 1;
-      currentRetryDelay = 0;
+    attempt = 1;
+    currentRetryDelay = 0;
+  }
+
+  public function getCurrentRetryDelay() {
+    return new Millis(currentRetryDelay);
   }
 
   public function increase() {
-      if (attempt > 10) {
-          if (currentRetryDelay < 60_000) {
-              if (currentRetryDelay * 2 < 60_000) {
-                  currentRetryDelay *= 2;
-              } else {
-                  currentRetryDelay = 60_000;
-              }
-          }
-      } else {
-          attempt += 1;
+    if (attempt > 10) {
+      if (currentRetryDelay < 60_000) {
+        if (currentRetryDelay * 2 < 60_000) {
+          currentRetryDelay *= 2;
+        } else {
+          currentRetryDelay = 60_000;
+        }
       }
+    } else {
+      attempt += 1;
+    }
   }
 
   public function reset(retryDelay: Long) {
-      attempt = 1;
-      currentRetryDelay = retryDelay;
+    attempt = 1;
+    currentRetryDelay = retryDelay;
   }
 }
