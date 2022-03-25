df9acb63977c523d55a12df755844e6e585c1fd8
acarioni <alessandro.carioni@lightstreamer.com>
2022-03-25 10:06:20 +0100

    add retry delay logic

---
+++ src/common/com/lightstreamer/client/internal/RetryDelayCounter.hx
@@ -0,0 +1,32 @@
+package com.lightstreamer.client.internal;
+
+import com.lightstreamer.internal.NativeTypes.Long;
+
+class RetryDelayCounter {
+  var attempt: Int;
+  var currentRetryDelay: Long;
+
+  public function new() {
+      attempt = 1;
+      currentRetryDelay = 0;
+  }
+
+  public function increase() {
+      if (attempt > 10) {
+          if (currentRetryDelay < 60_000) {
+              if (currentRetryDelay * 2 < 60_000) {
+                  currentRetryDelay *= 2;
+              } else {
+                  currentRetryDelay = 60_000;
+              }
+          }
+      } else {
+          attempt += 1;
+      }
+  }
+
+  public function reset(retryDelay: Long) {
+      attempt = 1;
+      currentRetryDelay = retryDelay;
+  }
+}
