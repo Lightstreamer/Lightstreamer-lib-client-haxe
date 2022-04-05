a01405020e4331166c32386173cab98e7d151544
acarioni <alessandro.carioni@lightstreamer.com>
2022-04-05 08:39:19 +0200

    add utility macros

---
+++ src/common/com/lightstreamer/internal/MacroTools.hx
@@ -0,0 +1,22 @@
+package com.lightstreamer.internal;
+
+import haxe.macro.Expr;
+using haxe.macro.Tools;
+
+macro function assert(e: Expr) {
+  var expr = e.toString();
+  var pos = e.pos;
+  return macro if (!$e) @:pos(pos) throw new haxe.Exception("Assertion failure: " + $v{expr});
+}
+
+macro function bypassAccessors(e) 
+  return _bypassAccessors(e);
+
+private function _bypassAccessors(e: Expr) {
+  return switch e.expr {
+    case EBinop(OpAssign, {expr: EField(_, _)}, _):
+      {expr: EMeta({pos: e.pos, name: ":bypassAccessor"}, e), pos: e.pos};
+    case _:
+      e.map(_bypassAccessors);
+  }
+}
