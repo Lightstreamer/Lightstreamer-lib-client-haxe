cd57afef3c12353dc142f7407d3796823eee62ca
acarioni <alessandro.carioni@lightstreamer.com>
2022-04-29 12:20:24 +0200

    implement sub manager (wip)

--- src/common/com/lightstreamer/client/internal/SubscriptionManager.hx
+++ src/common/com/lightstreamer/client/internal/SubscriptionManager.hx
@@ -4,38 +4,22 @@ import com.lightstreamer.internal.Types;
 import com.lightstreamer.client.internal.ClientRequests;
 
 // TODO SubscriptionManager
-class SubscriptionManager implements Encodable {
+interface SubscriptionManager extends Encodable {
   public final subId: Int;
 
-  public function new() {
-    // TODO
-    this.subId = 0;
-  }
+  public function evtU(itemIdx: Pos, values: Map<Pos, FieldValue>): Void;
+  public function evtSUBOK(nItems: Int, nFields: Int): Void;
+  public function evtSUBCMD(nItems: Int, nFields: Int, keyIdx: Pos, cmdIdx: Pos): Void;
+  public function evtUNSUB(): Void;
+  public function evtEOS(itemIdx: Pos): Void;
+  public function evtCS(itemIdx: Pos): Void;
+  public function evtOV(itemIdx: Pos, lostUpdates: Int): Void;
+  public function evtCONF(freq: RealMaxFrequency): Void;
+  public function evtREQOK(reqId: Int): Void;
+  public function evtREQERR(reqId: Int, errorCode: Int, errorMsg: String): Void;
+  public function evtExtAbort(): Void;
 
-  public function evtU(itemIdx: Pos, values: Map<Pos, FieldValue>) {}
-  public function evtSUBOK(nItems: Int, nFields: Int) {}
-  public function evtSUBCMD(nItems: Int, nFields: Int, keyIdx: Pos, cmdIdx: Pos) {}
-  public function evtUNSUB() {}
-  public function evtEOS(itemIdx: Pos) {}
-  public function evtCS(itemIdx: Pos) {}
-  public function evtOV(itemIdx: Pos, lostUpdates: Int) {}
-  public function evtCONF(freq: RealMaxFrequency) {}
-  public function evtREQOK(reqId: Int) {}
-  public function evtREQERR(reqId: Int, errorCode: Int, errorMsg: String) {}
-  public function evtExtAbort() {}
-
-	public function isPending():Bool {
-    // TODO
-		throw new haxe.exceptions.NotImplementedException();
-	}
-
-	public function encode(isWS:Bool):String {
-    // TODO
-		throw new haxe.exceptions.NotImplementedException();
-	}
-
-	public function encodeWS():String {
-    // TODO
-		throw new haxe.exceptions.NotImplementedException();
-	}
+	public function isPending(): Bool;
+	public function encode(isWS:Bool): String;
+	public function encodeWS(): String;
 }
--- src/common/com/lightstreamer/client/internal/SubscriptionManagerZombie.hx
+++ src/common/com/lightstreamer/client/internal/SubscriptionManagerZombie.hx
@@ -1,15 +1,165 @@
 package com.lightstreamer.client.internal;
 
+import com.lightstreamer.internal.RequestBuilder;
+import com.lightstreamer.internal.RLock;
 import com.lightstreamer.internal.Types;
+import com.lightstreamer.internal.NativeTypes;
+import com.lightstreamer.log.LoggerTools;
+using com.lightstreamer.log.LoggerTools;
 
-// TODO SubscriptionManagerZombie
-class SubscriptionManagerZombie {
-  public function new(subId: Int, clientSM: ClientMachine) {}
-  public function evtU(itemIdx: Pos, values: Map<Pos, FieldValue>) {}
-  public function evtSUBOK(nItems: Int, nFields: Int) {}
-  public function evtSUBCMD(nItems: Int, nFields: Int, keyIdx: Pos, cmdIdx: Pos) {}
-  public function evtEOS(itemIdx: Pos) {}
-  public function evtCS(itemIdx: Pos) {}
-  public function evtOV(itemIdx: Pos, lostUpdates: Int) {}
-  public function evtCONF(freq: RealMaxFrequency) {}
+private enum abstract State_m(Int) {
+  var s1 = 1; var s2 = 2; var s3 = 3;
+}
+
+// TODO synchronize method of ClientMachine accessed by SubscriptionManagerZombie
+@:access(com.lightstreamer.client.internal.ClientMachine)
+@:build(com.lightstreamer.internal.Macros.synchronizeClass())
+class SubscriptionManagerZombie implements SubscriptionManager {
+  public final subId: Int;
+  var s_m: State_m = s1;
+  final lock: RLock;
+  var m_lastDeleteReqId: Null<Int>;
+  final m_client: ClientMachine;
+
+  public function new(subId: Int, client: ClientMachine) {
+    this.subId = subId;
+    this.lock = client.lock;
+    this.m_client = client;
+    client.relateSubManager(this);
+  }
+
+  function finalize() {
+    m_client.unrelateSubManager(this);
+  }
+
+  public function evtExtAbort() {
+    traceEvent("abort");
+    if (s_m == s2) {
+      finalize();
+      goto(s3);
+    }
+  }
+
+  public function evtREQERR(reqId: Int, code: Int, msg: String) {
+    traceEvent("REQERR");
+    if (s_m == s2 && reqId == m_lastDeleteReqId) {
+      finalize();
+      goto(s3);
+    }
+  }
+
+  public function evtREQOK(reqId: Int) {
+    traceEvent("REQOK");
+    if (s_m == s2 && reqId == m_lastDeleteReqId) {
+      finalize();
+      goto(s3);
+    }
+  }
+
+  public function evtSUBOK(nItems: Int, nFields: Int) {
+    traceEvent("SUBOK");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public function evtSUBCMD(nItems: Int, nFields: Int, keyIdx: Int, cmdIdx: Int) {
+    traceEvent("SUBCMD");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public function evtUNSUB() {
+    traceEvent("UNSUB");
+    if (s_m == s2) {
+      finalize();
+      goto(s3);
+    }
+  }
+  
+  public function evtU(itemIdx: Pos, values: Map<Pos, FieldValue>) {
+    traceEvent("U");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public function evtEOS(itemIdx: Pos) {
+    traceEvent("EOS");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public function evtCS(itemIdx: Pos) {
+    traceEvent("CS");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public  function evtOV(itemIdx: Pos, lostUpdates: Int) {
+    traceEvent("OV");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+  
+  public function evtCONF(freq: RealMaxFrequency) {
+    traceEvent("CONF");
+    if (s_m == s1) {
+      goto(s2);
+      genSendControl();
+    }
+  }
+
+  public function isPending(): Bool {
+    return s_m == s2;
+  }
+  
+  public function encode(isWS: Bool): String {
+    if (isPending()) {
+      return encodeDelete(isWS);
+    } else {
+      throw new IllegalStateException("Can't encode unsubscription request");
+    }
+  }
+  
+  public function encodeWS(): String {
+    return "control\r\n" + encode(true);
+  }
+  
+  function genSendControl() {
+    m_client.evtSendControl(this);
+  }
+
+  function encodeDelete(isWS: Bool): String {
+    var req = new RequestBuilder();
+    m_lastDeleteReqId = m_client.generateFreshReqId();
+    req.LS_reqId(m_lastDeleteReqId);
+    req.LS_subId(subId);
+    req.LS_op("delete");
+    if (isWS) {
+      req.LS_ack(false);
+    }
+    req.LS_cause("zombie");
+    protocolLogger.logInfo('Sending Subscription delete: $req');
+    return req.getEncodedString();
+  }
+
+  function traceEvent(evt: String) {
+    internalLogger.logTrace('zsub#$evt($subId) in $s_m');
+  }
+
+  function goto(target: State_m) {
+    s_m = target;
+    internalLogger.logTrace('zsub#goto($subId) $s_m');
+  }
 }
