5a6efd3e26cf30d0eb1a5a47d832eb494c115154
acarioni <alessandro.carioni@lightstreamer.com>
2022-05-19 08:41:02 +0200

    adjust factory methods

--- src/common/com/lightstreamer/internal/Factories.hx
+++ src/common/com/lightstreamer/internal/Factories.hx
@@ -7,20 +7,30 @@ function createWsClient(url: String, headers: Null<Map<String, String>>,
   onOpen: IWsClient->Void,
   onText: (IWsClient, String)->Void, 
   onError: (IWsClient, String)->Void): IWsClient {
+  #if java
   // TODO pass all parameters
   return new WsClient(url, headers, null, null, onOpen, onText, onError);
+  #elseif (js && LS_WEB)
+  return new WsClient(url, onOpen, onText, onError);
+  #elseif js
+  return new WsClient(url, headers, onOpen, onText, onError);
+  #end
 }
 
 function createHttpClient(url: String, body: String, headers: Null<Map<String, String>>,
   onText: (IHttpClient, String)->Void, 
   onError: (IHttpClient, String)->Void, 
   onDone: IHttpClient->Void): IHttpClient {
+  #if java
   // TODO pass all parameters
   return new HttpClient(url, body, headers, null, null, onText, onError, onDone);
+  #elseif js
+  return new HttpClient(url, body, headers, onText, onError, onDone);
+  #end
 }
 
 function createReachabilityManager(host: String): IReachability {
-  return new NetworkReachabilityManager();
+  return new DummyReachabilityManager();
 }
 
 function createTimer(id: String, delay: Millis, callback: ITimer->Void): ITimer {
