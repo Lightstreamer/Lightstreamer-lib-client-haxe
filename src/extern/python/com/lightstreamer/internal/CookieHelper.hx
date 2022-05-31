3d95f96aa92cf6e3538d8f3856880837cb70b634
acarioni <alessandro.carioni@lightstreamer.com>
2022-05-31 08:21:36 +0200

    refactor cookies code

--- src/extern/python/com/lightstreamer/internal/CookieHelper.hx
+++ src/extern/python/com/lightstreamer/internal/CookieHelper.hx
@@ -2,6 +2,8 @@ package com.lightstreamer.internal;
 
 @:pythonImport("com_lightstreamer", "CookieHelper")
 extern class CookieHelper {
+  static var instance(get, never): CookieHelper;
+  inline static function get_instance() return getInstance();
   static function getInstance(): CookieHelper;
   function addCookies(uri: String, cookies: SimpleCookie): Void;
   function getCookies(uri: Null<String>): SimpleCookie;
