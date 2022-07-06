9455216542ebcb2fadb44c38c83898f250a8e269
acarioni <alessandro.carioni@lightstreamer.com>
2022-07-06 17:04:13 +0200

    fix typos

--- tools/release/python/Distribute.hx
+++ tools/release/python/Distribute.hx
@@ -3,11 +3,11 @@ import Sys.println;
 function main() {
   // NB the current directory must be the root of the project
   println("
-To upload the package on Test PyPi, run the following commands (from the folder 'dist/pythlightstreamer-client'):
+To upload the package on Test PyPi, run the following commands (from the folder 'dist/python/lightstreamer-client'):
 $ python3 -m build
 $ twine upload -r testpypi dist/*
 
-To upload the package on PyPi, run the following commands (from the folder 'dist/pythlightstreamer-client'):
+To upload the package on PyPi, run the following commands (from the folder 'dist/python/lightstreamer-client'):
 $ python3 -m build
 $ twine upload dist/*
 
