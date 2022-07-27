fa440da1a69f574047071fa1ac28f67d99ad1698
acarioni <alessandro.carioni@lightstreamer.com>
2022-07-27 09:21:01 +0200

    increment TLCP

--- src/common/com/lightstreamer/internal/Constants.hx
+++ src/common/com/lightstreamer/internal/Constants.hx
@@ -1,6 +1,6 @@
 package com.lightstreamer.internal;
 
-final TLCP_VERSION = "TLCP-2.3.0";
+final TLCP_VERSION = "TLCP-2.4.0";
 final FULL_TLCP_VERSION = TLCP_VERSION + ".lightstreamer.com";
 
 #if LS_TEST
