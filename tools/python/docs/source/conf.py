4e4bb1f3896a52cf5771f94393cc58c050804c00
acarioni <alessandro.carioni@lightstreamer.com>
2022-11-11 08:16:01 +0100

    adjust python docs generation

--- tools/python/docs/source/conf.py
+++ tools/python/docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../lightstreamer/client/'))
+sys.path.insert(0, os.path.abspath('../../../../bin/python/build/lib/lightstreamer-client/src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
