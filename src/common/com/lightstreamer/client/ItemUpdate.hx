0e8414c57ac327a7f2af61c35e70c866ee665972
acarioni <alessandro.carioni@lightstreamer.com>
2022-12-14 17:08:56 +0100

    indent code

--- src/common/com/lightstreamer/client/ItemUpdate.hx
+++ src/common/com/lightstreamer/client/ItemUpdate.hx
@@ -15,48 +15,48 @@ extern interface ItemUpdate {
   var Snapshot(get, never): Bool;
   #end
   #if static
-  #if cpp
-  function getValue(fieldName: String): Null<String>;
-  function getValueWithFieldPos(fieldPos: Int): Null<String>;
-  function isValueChanged(fieldName: String): Bool;
-  function isValueChangedWithFieldPos(fieldPos: Int): Bool;
-  #else
-  overload function getValue(fieldName: String): Null<String>;
-  overload function getValue(fieldPos: Int): Null<String>;
-  overload function isValueChanged(fieldName: String): Bool;
-  overload function isValueChanged(fieldPos: Int): Bool;
-  #if LS_JSON_PATCH
-  overload function getValueAsJSONPatchIfAvailable(fieldName: String): Null<String>;
-  overload function getValueAsJSONPatchIfAvailable(fieldPos: Int): Null<String>;
-  #end
-  #end
+    #if cpp
+    function getValue(fieldName: String): Null<String>;
+    function getValueWithFieldPos(fieldPos: Int): Null<String>;
+    function isValueChanged(fieldName: String): Bool;
+    function isValueChangedWithFieldPos(fieldPos: Int): Bool;
+    #else
+    overload function getValue(fieldName: String): Null<String>;
+    overload function getValue(fieldPos: Int): Null<String>;
+    overload function isValueChanged(fieldName: String): Bool;
+    overload function isValueChanged(fieldPos: Int): Bool;
+      #if LS_JSON_PATCH
+      overload function getValueAsJSONPatchIfAvailable(fieldName: String): Null<String>;
+      overload function getValueAsJSONPatchIfAvailable(fieldPos: Int): Null<String>;
+      #end
+    #end
   #else
   function getValue(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Null<String>;
   function isValueChanged(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Bool;
-  #if LS_JSON_PATCH
-  #if js
-  function getValueAsJSONPatchIfAvailable(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Null<com.lightstreamer.internal.patch.Json.JsonPatch>;
-  #else
-  function getValueAsJSONPatchIfAvailable(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Null<String>;
-  #end
-  #end
+    #if LS_JSON_PATCH
+      #if js
+      function getValueAsJSONPatchIfAvailable(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Null<com.lightstreamer.internal.patch.Json.JsonPatch>;
+      #else
+      function getValueAsJSONPatchIfAvailable(fieldNameOrPos: haxe.extern.EitherType<String, Int>): Null<String>;
+      #end
+    #end
   #end
   #if js
   function forEachChangedField(iterator: (fieldName: Null<String>, fieldPos: Int, value: Null<String>) -> Void): Void;
   function forEachField(iterator: (fieldName: Null<String>, fieldPos: Int, value: Null<String>) -> Void): Void;
   #end
   #if (!js || LS_TEST)
-  #if !cs
-  function getChangedFields(): NativeStringMap<Null<String>>;
-  function getChangedFieldsByPosition(): NativeIntMap<Null<String>>;
-  function getFields(): NativeStringMap<Null<String>>;
-  function getFieldsByPosition(): NativeIntMap<Null<String>>;
-  #else
-  var ChangedFields(get, never): NativeStringMap<Null<String>>;
-  var ChangedFieldsByPosition(get, never): NativeIntMap<Null<String>>;
-  var Fields(get, never): NativeStringMap<Null<String>>;
-  var FieldsByPosition(get, never): NativeIntMap<Null<String>>;
-  #end
+    #if !cs
+    function getChangedFields(): NativeStringMap<Null<String>>;
+    function getChangedFieldsByPosition(): NativeIntMap<Null<String>>;
+    function getFields(): NativeStringMap<Null<String>>;
+    function getFieldsByPosition(): NativeIntMap<Null<String>>;
+    #else
+    var ChangedFields(get, never): NativeStringMap<Null<String>>;
+    var ChangedFieldsByPosition(get, never): NativeIntMap<Null<String>>;
+    var Fields(get, never): NativeStringMap<Null<String>>;
+    var FieldsByPosition(get, never): NativeIntMap<Null<String>>;
+    #end
   #end
 }
 
