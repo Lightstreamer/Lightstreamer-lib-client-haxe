3970f5c76eca7d4346adfbcb29db3b74bdb0800a
acarioni <alessandro.carioni@lightstreamer.com>
2024-01-18 17:35:02 +0100

    use queueMicrotask when available

--- src/platform/web/com/lightstreamer/internal/Timers.hx
+++ src/platform/web/com/lightstreamer/internal/Timers.hx
@@ -1,25 +1,29 @@
 package com.lightstreamer.internal;
 
 import com.lightstreamer.internal.WorkerTimer;
+import com.lightstreamer.internal.Globals.instance as globals;
 
 typedef TimeoutHandle = Int;
 
 // fall-back to standard setTimeout in environments not supporting web workers (e.g. react native)
-private final hasWorkers = js.Lib.typeof(js.html.Worker) != "undefined";
-private final _setTimeout: (callback: ()->Void, delay: Int) -> TimeoutHandle = hasWorkers ? WorkerTimer.setTimeout : cast js.Browser.window.setTimeout;
-private final _clearTimeout: (handle: TimeoutHandle) -> Void = hasWorkers ? WorkerTimer.clearTimeout : js.Browser.window.clearTimeout;
+private final _setTimeout: (callback: ()->Void, delay: Int) -> TimeoutHandle = globals.hasWorkers ? WorkerTimer.setTimeout : js.Lib.global.setTimeout;
+private final _clearTimeout: (handle: TimeoutHandle) -> Void = globals.hasWorkers ? WorkerTimer.clearTimeout : js.Lib.global.clearTimeout;
+private final _setImmediate: (callback: ()->Void) -> Void = globals.hasMicroTasks ? js.Lib.global.queueMicrotask : (callback: ()->Void) -> {
+  // ensure callbacks are called in the same order of _setImmediate invocations
+  queue.push(callback);
+  _setTimeout(handleQueue, 0);
+};
 
 inline function setTimeout(callback: ()->Void, delay: Int): TimeoutHandle {
   return _setTimeout(callback, delay);
 }
 
-inline function clearTimeout(handle: TimeoutHandle) {
+inline function clearTimeout(handle: TimeoutHandle): Void {
   _clearTimeout(handle);
 }
 
-function setImmediate(callback: ()->Void): Void {
-  queue.push(callback);
-  _setTimeout(handleQueue, 0);
+inline function setImmediate(callback: ()->Void): Void {
+  _setImmediate(callback);
 }
 
 private final queue: Array<()->Void> = [];
