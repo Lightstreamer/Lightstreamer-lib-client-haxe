09df7794c4bf13c3dec277f0098c141557a0c5ce
acarioni <alessandro.carioni@lightstreamer.com>
2024-05-20 08:21:29 +0200

    simplify Proxy ctor

--- src/wrapper/cpp/include/Lightstreamer/Proxy.h
+++ src/wrapper/cpp/include/Lightstreamer/Proxy.h
@@ -12,13 +12,7 @@ struct Proxy {
   std::string user;
   std::string password;
 
-  Proxy(const std::string& type, const std::string& host, int port) :
-    Proxy(type, host, port, "", "") {}
-
-  Proxy(const std::string& type, const std::string& host, int port, const std::string& user) :
-    Proxy(type, host, port, user, "") {}
-
-  Proxy(const std::string& type, const std::string& host, int port, const std::string& user, const std::string& password) :
+  Proxy(const std::string& type, const std::string& host, int port, const std::string& user = "", const std::string& password = "") :
     type(type), host(host), port(port), user(user), password(password) {}
 };
 
