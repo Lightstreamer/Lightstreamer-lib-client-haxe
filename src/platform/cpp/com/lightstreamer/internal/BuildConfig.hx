ec9b4b6f77c0b180dbeadef898c922cc50e1214f
acarioni <alessandro.carioni@lightstreamer.com>
2024-05-29 15:11:32 +0200

    fix build path

--- src/platform/cpp/com/lightstreamer/internal/BuildConfig.hx
+++ src/platform/cpp/com/lightstreamer/internal/BuildConfig.hx
@@ -1,5 +1,5 @@
 package com.lightstreamer.internal;
 
 @:keep
-@:buildXml("<include name='../../src-hxpoco/hxpoco.xml'/>")
+@:buildXml("<include name='../../src/wrapper/cpp/wrapper.xml'/>") // path is relative to folder bin/cpp
 class BuildConfig {}
