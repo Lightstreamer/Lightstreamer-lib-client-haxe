4d764037bd0cedbe2f6f72a5581f22295be07a93
acarioni <alessandro.carioni@lightstreamer.com>
2024-05-30 17:20:49 +0200

    add basic demo test

---
+++ test-system/cpp/test_demo.cpp
@@ -0,0 +1,34 @@
+#include "Lightstreamer/LightstreamerClient.h"
+#include "Lightstreamer/SubscriptionListener.h"
+#include "Lightstreamer/ConsoleLoggerProvider.h"
+#include <iostream>
+
+using namespace Lightstreamer;
+
+class MySubscriptionListener: public SubscriptionListener {
+public:
+  void onItemUpdate(ItemUpdate& update) override {
+    std::cout << update.getValue("stock_name") << ": " << update.getValue("last_price") << std::endl;
+  }
+};
+
+int main() {
+  LightstreamerClient::initialize([](const char* info) {
+    std::cout << "UNCAUGHT EXCEPTION: " << info << "\n";
+    std::cout << "TERMINATING THE PROCESS...\n";
+    exit(255);
+  });
+  LightstreamerClient::setLoggerProvider(new ConsoleLoggerProvider(ConsoleLogLevel::WARN));
+
+  LightstreamerClient client("https://push.lightstreamer.com/","DEMO");
+  client.connect();
+
+  Subscription sub("MERGE", {"item1","item2","item3"}, {"stock_name","last_price"});
+  sub.setDataAdapter("QUOTE_ADAPTER");
+  sub.setRequestedSnapshot("yes");
+  sub.addListener(new MySubscriptionListener());
+  client.subscribe(&sub);
+
+  std::string s;
+  std::cin >> s;
+}
