7a028b8db6e42cfad870231db903742e2e7b7d02
acarioni <alessandro.carioni@lightstreamer.com>
2024-05-30 08:43:21 +0200

    clean up

--- src-hxpoco/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-g++ -g src/test-bug-ws2.cpp -Iinclude -I${POCO_BASE}/Foundation/include -I${POCO_BASE}/Net/include -I/opt/homebrew/opt/openssl/include -L${POCO_BASE}/lib/Darwin/arm64 -lPocoFoundationd -lPocoNetd -lPocoNetSSLd -std=c++17 -o bin/a.out
-
-bin/a.out
-
