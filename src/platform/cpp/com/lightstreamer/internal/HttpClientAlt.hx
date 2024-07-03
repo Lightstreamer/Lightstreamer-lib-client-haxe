bea98428e1156a121392391acde6992a35007870
acarioni <alessandro.carioni@lightstreamer.com>
2024-07-03 10:01:04 +0200

    use haxe native http and ws

---
+++ src/platform/cpp/com/lightstreamer/internal/HttpClientAlt.hx
@@ -0,0 +1,264 @@
+package com.lightstreamer.internal;
+
+import haxe.atomic.AtomicBool;
+import sys.Http;
+import sys.thread.Thread;
+import com.lightstreamer.client.Proxy.LSProxy as Proxy;
+import com.lightstreamer.internal.PlatformApi.IHttpClient;
+import com.lightstreamer.log.LoggerTools;
+
+using com.lightstreamer.log.LoggerTools;
+
+class HttpClientAlt implements IHttpClient {
+  final _thread: Thread;
+  final _disposed = new AtomicBool(false);
+
+  public function new(url: String, body: String, 
+    headers: Null<Map<String, String>>,
+    proxy: Null<Proxy>,
+    _onText: (HttpClientAlt, String)->Void, 
+    _onError: (HttpClientAlt, String)->Void, 
+    _onDone: HttpClientAlt->Void) 
+  {
+    streamLogger.logDebug('HTTP sending: $url $body headers($headers) proxy($proxy)');
+    _thread = Thread.create(() -> {
+			var req = new AsyncHttp(url);
+			req.onData = line -> {
+        if (isDisposed()) {
+          return;
+        }
+        streamLogger.logDebug('HTTP event: text($line)');
+        _onText(this, line);
+      }
+			req.onError = error -> {
+        if (isDisposed()) {
+          return;
+        }
+        streamLogger.logDebug('HTTP event: error($error)');
+        _onError(this, error);
+      }
+      req.onDone = () -> {
+        if (isDisposed()) {
+          return;
+        }
+        streamLogger.logDebug("HTTP event: complete");
+        _onDone(this);
+      }
+			req.setPostData(body);
+			req.request(true);
+		});
+  }
+
+  public function dispose(): Void {
+    if (!_disposed.load()) {
+      streamLogger.logDebug("HTTP disposing");
+      _disposed.store(true);
+      _thread.sendMessage("close");
+    }
+  }
+
+  public function isDisposed(): Bool {
+    return _disposed.load();
+  }
+}
+
+private class AsyncHttp extends Http {
+  public dynamic function onDone() {}
+
+  override function success(data: haxe.io.Bytes) {
+    onDone();
+	}
+
+  @:nullSafety(Off)
+  override function readHttpResponse(api:haxe.io.Output, sock:sys.net.Socket) {
+		// READ the HTTP header (until \r\n\r\n)
+		var b = new haxe.io.BytesBuffer();
+		var k = 4;
+		var s = haxe.io.Bytes.alloc(4);
+		sock.setTimeout(cnxTimeout);
+		while (true) {
+			var p = 0;
+			while (p != k) {
+				try {
+					p += sock.input.readBytes(s, p, k - p);
+				}
+				catch (e:haxe.io.Eof) { }
+			}
+			b.addBytes(s, 0, k);
+			switch (k) {
+				case 1:
+					var c = s.get(0);
+					if (c == 10)
+						break;
+					if (c == 13)
+						k = 3;
+					else
+						k = 4;
+				case 2:
+					var c = s.get(1);
+					if (c == 10) {
+						if (s.get(0) == 13)
+							break;
+						k = 4;
+					} else if (c == 13)
+						k = 3;
+					else
+						k = 4;
+				case 3:
+					var c = s.get(2);
+					if (c == 10) {
+						if (s.get(1) != 13)
+							k = 4;
+						else if (s.get(0) != 10)
+							k = 2;
+						else
+							break;
+					} else if (c == 13) {
+						if (s.get(1) != 10 || s.get(0) != 13)
+							k = 1;
+						else
+							k = 3;
+					} else
+						k = 4;
+				case 4:
+					var c = s.get(3);
+					if (c == 10) {
+						if (s.get(2) != 13)
+							continue;
+						else if (s.get(1) != 10 || s.get(0) != 13)
+							k = 2;
+						else
+							break;
+					} else if (c == 13) {
+						if (s.get(2) != 10 || s.get(1) != 13)
+							k = 3;
+						else
+							k = 1;
+					}
+			}
+		}
+		#if neko
+		var headers = neko.Lib.stringReference(b.getBytes()).split("\r\n");
+		#else
+		var headers = b.getBytes().toString().split("\r\n");
+		#end
+		var response = headers.shift();
+		var rp = response.split(" ");
+		var status = Std.parseInt(rp[1]);
+		if (status == 0 || status == null)
+			throw "Response status error";
+
+		// remove the two lasts \r\n\r\n
+		headers.pop();
+		headers.pop();
+		responseHeaders = new haxe.ds.StringMap();
+		var size = null;
+		var chunked = false;
+		for (hline in headers) {
+			var a = hline.split(": ");
+			var hname = a.shift();
+			var hval = if (a.length == 1) a[0] else a.join(": ");
+			hval = StringTools.ltrim(StringTools.rtrim(hval));
+
+			{
+				var previousValue = responseHeaders.get(hname);
+				if (previousValue != null) {
+					if (responseHeadersSameKey == null) {
+						responseHeadersSameKey = new haxe.ds.Map<String, Array<String>>();
+					}
+					var array = responseHeadersSameKey.get(hname);
+					if (array == null) {
+						array = new Array<String>();
+						array.push(previousValue);
+						responseHeadersSameKey.set(hname, array);
+					}
+					array.push(hval);
+				}
+			}
+			responseHeaders.set(hname, hval);
+			switch (hname.toLowerCase()) {
+				case "content-length":
+					size = Std.parseInt(hval);
+				case "transfer-encoding":
+					chunked = (hval.toLowerCase() == "chunked");
+			}
+		}
+
+		onStatus(status);
+
+		var chunk_re = ~/^([0-9A-Fa-f]+)[ ]*\r\n/m;
+		chunk_size = null;
+		chunk_buf = null;
+
+		var bufsize = 1024;
+		var buf = haxe.io.Bytes.alloc(bufsize);
+		if (chunked) {
+			// try {
+			// 	while (true) {
+			// 		var len = sock.input.readBytes(buf, 0, bufsize);
+			// 		if (!readChunk(chunk_re, api, buf, len))
+			// 			break;
+			// 	}
+			// } catch (e:haxe.io.Eof) {
+			// 	throw "Transfer aborted";
+			// }
+
+      // BEGIN PATCH
+      throw new haxe.Exception("Chunked encoding not supported");
+      // END PATCH
+		} else if (size == null) {
+			// if (!noShutdown)
+			// 	sock.shutdown(false, true);
+			// try {
+			// 	while (true) {
+			// 		var len = sock.input.readBytes(buf, 0, bufsize);
+			// 		if (len == 0)
+			// 			break;
+			// 		api.writeBytes(buf, 0, len);
+			// 	}
+			// } catch (e:haxe.io.Eof) {}
+
+      // BEGIN PATCH
+      throw new haxe.Exception("Unlimited length not supported");
+      // END PATCH
+		} else {
+			// api.prepare(size);
+			// try {
+			// 	while (size > 0) {
+			// 		var len = sock.input.readBytes(buf, 0, if (size > bufsize) bufsize else size);
+			// 		api.writeBytes(buf, 0, len);
+			// 		size -= len;
+			// 	}
+			// } catch (e:haxe.io.Eof) {
+			// 	throw "Transfer aborted";
+			// }
+
+      // BEGIN PATCH
+      sock.setTimeout(0.1); // throws an Error.Blocked when the read timeout expires but no data is available
+      while (true) {
+        try {
+          var s = sock.input.readLine();
+          onData(s);
+        } catch(ex: haxe.io.Error) {
+          if (ex != haxe.io.Error.Blocked) {
+            throw ex; 
+          }
+          var m = Thread.readMessage(false);
+          if (m != null && m == "close") {
+            // forced closing
+            break;
+          }
+        } catch(ex: haxe.io.Eof) {
+          // all data has been read
+          break;
+        }
+      }
+      // END PATCH
+		}
+		if (chunked && (chunk_size != null || chunk_buf != null))
+			throw "Invalid chunk";
+		if (status < 200 || status >= 400)
+			throw "Http Error #" + status;
+		api.close();
+	}
+}
---
+++ src/platform/cpp/com/lightstreamer/internal/WsClientAlt.hx
@@ -0,0 +1,103 @@
+package com.lightstreamer.internal;
+
+import haxe.net.WebSocket;
+import haxe.atomic.AtomicBool;
+import sys.Http;
+import sys.thread.Thread;
+import com.lightstreamer.client.Proxy.LSProxy as Proxy;
+import com.lightstreamer.internal.PlatformApi.IWsClient;
+import com.lightstreamer.log.LoggerTools;
+
+using com.lightstreamer.log.LoggerTools;
+
+class WsClientAlt implements IWsClient {
+  final _thread: Thread;
+  final _disposed = new AtomicBool(false);
+
+  public function new(url: String, 
+    headers: Null<Map<String, String>>,
+    proxy: Null<Proxy>,
+    _onOpen: WsClientAlt->Void,
+    _onText: (WsClientAlt, String)->Void, 
+    _onError: (WsClientAlt, String)->Void)
+  {
+    streamLogger.logDebug('WS connecting: $url headers($headers) proxy($proxy)');
+    _thread = Thread.create(() -> {
+      try {
+        url = ~/^http/.replace(url, "ws");
+        var ws = WebSocket.create(url, [Constants.FULL_TLCP_VERSION], false);
+        ws.onopen = () -> {
+          if (isDisposed()) {
+            return;
+          }
+          streamLogger.logDebug('WS event: open');
+          _onOpen(this);
+        }
+        ws.onmessageString = text -> {
+          if (isDisposed()) {
+            return;
+          }
+          for (line in text.split("\r\n")) {
+            if (isDisposed()) {
+              return;
+            }
+            if (line == "") continue;
+            streamLogger.logDebug('WS event: text($line)');
+            _onText(this, line);
+          }
+        }
+        ws.onerror = error -> {
+          if (isDisposed()) {
+            return;
+          }
+          streamLogger.logDebug('WS event: error($error)');
+          _onError(this, error);
+        }
+        while (!_disposed.load()) {
+          ws.process();
+          var m: Null<OpCode> = Thread.readMessage(false);
+          if (m != null) {
+            switch (m) {
+            case WsSend(msg):
+              ws.sendString(msg);
+            case WsClose:
+              break;
+            }
+          }
+          Sys.sleep(0.1);
+        }
+        ws.close();
+      } catch(ex) {
+        if (isDisposed()) {
+          return;
+        }
+        streamLogger.logErrorEx('WS event: error(${ex.message})', ex);
+        _onError(this, ex.message);
+      }
+    });
+  }
+
+  public function dispose(): Void {
+    if (!_disposed.load()) {
+      streamLogger.logDebug("WS disposing");
+      _disposed.store(true);
+      _thread.sendMessage(WsClose);
+    }
+  }
+
+  public function isDisposed(): Bool {
+    return _disposed.load();
+  }
+
+  public function send(txt: String): Void {
+    if (!_disposed.load()) {
+      streamLogger.logDebug('WS sending: $txt');
+      _thread.sendMessage(WsSend(txt));
+    }
+  }
+}
+
+private enum OpCode {
+  WsSend(msg: String);
+  WsClose;
+}
