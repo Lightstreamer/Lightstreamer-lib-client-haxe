cdfccc0e06082602fa68f342669d2c564cb3b0b4
acarioni <alessandro.carioni@lightstreamer.com>
2025-04-16 17:40:30 +0200

    Add copyright notice

--- src-log/LoggerProxy.js
+++ src-log/LoggerProxy.js
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) Lightstreamer Srl
+  Copyright (c) 2023 Lightstreamer Srl
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
--- src/wrapper/js/mpn/MpnDevice.js
+++ src/wrapper/js/mpn/MpnDevice.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
   /**
 	 * Creates an object to be used to describe an MPN device that is going to be registered to the MPN Module of Lightstreamer Server.<BR>
      * During creation the MpnDevice tries to acquires any previously registered device token from localStorage.
