614c5f4975a943ea53a4a95f181b649b206af724
acarioni <alessandro.carioni@lightstreamer.com>
2026-08-13 14:29:25 +0200

    Align java se and android tests

--- test-system/android/haxe-smoke-tests/app/build.gradle.kts
+++ test-system/android/haxe-smoke-tests/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 }
 
 dependencies {
-    implementation("com.lightstreamer:ls-android-client:5.3.2-snapshot")
+    implementation("com.lightstreamer:ls-android-client:5.3.3-snapshot.1")
     implementation("net.jodah:concurrentunit:0.4.6")
     implementation("com.google.android.gms:play-services-base:18.0.1") // Needed by MPN
 
